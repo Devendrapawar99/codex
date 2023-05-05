726c9595dc1e303d82fa5df95abeb278d931b0f2
Devendrapawar99 <devendrap575@gmail.com>
2023-05-06 00:22:38 +0530

    update the server

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
 
 
   //Fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-9lwz.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
